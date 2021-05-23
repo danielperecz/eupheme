33d435b03e36c5ec73677b733c783a6ac14f038c
pereczd2 <daniel.perecz2@mail.dcu.ie>
2021-05-23 19:43:43 +0100

    Connecting client with api

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  const apiUrl = "http://localhost:3001/api"
+  const apiCall = async () => {
+    const response = await axios.get(apiUrl);
+    setData(response.data.message);
+  }
+
+  useEffect(() => {
+    apiCall();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+          {data}
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
